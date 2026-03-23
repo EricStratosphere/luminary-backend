6c997ab3dc75df2cfaae6f4da04ae0728bf38bc5
Kali Live user <stratospherestu@gmail.com>
2026-03-23 20:53:36 +0800

    feat(model): Initialize model for bookmark schema.

---
+++ models/bookmark.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const bookmarkSchema = mongoose.Schema(
+    {
+        book_id : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'Book',
+            required : true,
+            index : true,
+        },
+        user_id : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'User',
+            required : true,
+            index : true,
+        }
+    }
+)
+
+const Bookmark = mongoose.model('Bookmark', bookmarkSchema);
+export default Bookmark;
