4c53aa2ae06bf52d43a3463d7d763f845f4520c7
Kali Live user <stratospherestu@gmail.com>
2026-03-30 12:46:28 +0800

    feat(database): Setup CORS

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import connectToDatabase from './database/mongodb.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+    origin : true
+}));
 
 app.disable('x-powered-by');
 
@@ -15,10 +20,9 @@ app.get('/', (req, res) => {
     return res.send("luminary backend active!");
 });
 
-
 app.listen(3000, async () => {
     await connectToDatabase();
-    console.log("luminary backend active!");
+    console.log("luminary backend active! at http://localhost:3000");
 });
 
 export default app;
