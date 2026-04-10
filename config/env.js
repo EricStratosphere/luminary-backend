8f8ce5212751c31876b72dd56d3ad7c466f468ed
Kali Live user <stratospherestu@gmail.com>
2026-04-10 16:36:58 +0800

    Export ACCESS_SECRET_TOKEN from config.js

--- config/env.js
+++ config/env.js
@@ -2,4 +2,4 @@ import {config} from 'dotenv';
 
 config({path : `.env.development.local`});
 
-export const { MONGODB_URI, PORT } = process.env;
+export const { MONGODB_URI, PORT, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
