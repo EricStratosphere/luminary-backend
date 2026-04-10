1fc3ee89bb9e40b714c0705314004575a6c7c735
Kali Live user <stratospherestu@gmail.com>
2026-04-10 09:40:39 +0800

    feat(auth): Implement routers for authentication

---
+++ controllers/authentication.controller.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+import jwt from 'jsonwebtoken'
+
+export const signUp = async (req, res) =>{
+
+}
+
+export const logIn = async()=>{
+
+}
+
+export const signOut = async(req, res) => {
+
+}
---
+++ routes/authentication.route.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { logIn, signOut, signUp } from "../controllers/authentication.controller.js";
+const authRouter = Router();
+
+
+authRouter.post('/login/', logIn);
+
+authRouter.post('/signout/', signOut);
+
+authRouter.post('/signup/', signUp);
+
+
+
+export default authRouter;
