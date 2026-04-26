9dbb1e61d588409c37ca1bc6df69d9e9be6ee913
Kali Live user <stratospherestu@gmail.com>
2026-04-26 10:16:51 +0800

    feat(gemini-api): Wrap database operations in prompt-text route within a mongoose transaction for atomicity.

--- controllers/gemini.controller.js
+++ controllers/gemini.controller.js
@@ -1,13 +1,36 @@
 import prompt from "../services/gemini-pro.js"
-export const promptText = (req, res) => {
+import mongoose from "mongoose";
+import Request from "../models/requests.model.js";
+import Response from "../models/response.model.js";
+export const promptText = async (req, res) => {
+    const session = await mongoose.startSession();
     try{
-        const result = prompt(req.body.prompt);
-        return res.status(200).json(
-            {
-                success : true,
-                response : result
+        await session.withTransaction(async() => {
+            const request = await Request.create({
+            book_id : req.body.book_id,
+            user_id : req.body.user_id,
+            request : req.body.prompt
+            });
+            if(!request){
+                throw new Error("Failed to process request!");
             }
-        )
+            const result = await prompt(req.body.prompt);
+            if(typeof result === 'string' && result.trim().length === 0){
+                throw new Error("No API response generated!");
+            }
+            const response = await Response.create({
+                book_id : req.body.book_id,
+                user_id : req.body.user_id,
+                response : result
+            });
+            res.status(200).json(
+                {
+                    success : true,
+                    requestData : request,
+                    responseData : response
+                }
+            )
+        })
     }
     catch(error){
         return res.status(500).json(
@@ -16,5 +39,17 @@ export const promptText = (req, res) => {
                 message : error.message
             }
         )
+    }
+    finally{
+        session.endSession();
+    }
+}
+
+export const getConversation = async (req, res) => {
+    try{
+        
+    }
+    catch(error){
+
     }
 }
