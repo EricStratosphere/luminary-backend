d0e2243b613818800985f76095bc47a97f50e1d0
Kali Live user <stratospherestu@gmail.com>
2026-06-03 19:02:54 +0800

    Isolate nodemailer credentials in .env

--- config/env.js
+++ config/env.js
@@ -2,4 +2,4 @@ import {config} from 'dotenv';
 
 config({path : `.env.development.local`});
 
-export const { MONGODB_URI, PORT, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET, GEMINI_API_KEY } = process.env;
+export const { MONGODB_URI, PORT, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET, GEMINI_API_KEY, GMAIL_ACCOUNT, GMAIL_APP_PASSWORD } = process.env;
